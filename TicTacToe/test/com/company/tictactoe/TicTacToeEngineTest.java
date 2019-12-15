c79ae3bef8e171ded64804a073c5bfd8eafe49d8
dwaffe <dwaffe@gmail.com>
2019-12-15 11:07:45 +0100

    pom

---
+++ TicTacToe/test/com/company/tictactoe/TicTacToeEngineTest.java
@@ -0,0 +1,26 @@
+package com.company.tictactoe;
+
+import com.company.tictactoe.board.piece.XPiece;
+import com.company.tictactoe.player.StrongAIPlayer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeEngineTest {
+
+    @Test
+    void runAiVsAi() {
+        assertTrue(true);
+//        assertTrue(false);
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
